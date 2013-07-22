e8d6de2e335af2f9b07329472bd8aa1f14e781f7
Kiet Lam <klam3518@gmail.com>
2013-07-22 11:30:17 -0700

    sample scons to build helloworld source into seperate directory

---
+++ scons-helloworld/src/main/hello.cc
@@ -0,0 +1,20 @@
+#include "hello.h"
+#include "../foofiles/foo.h"
+
+void Hello::HelloTest()
+{
+    //Helloworld:
+    std::string strTemp = "Hello World\n";
+    std::cout << strTemp;
+    
+    //Print out however many asterixes CFoo tells us to:
+    Foo fooTest;
+    int iTest = fooTest.GetTheNumber();
+    
+    for(int i = 0; i < iTest; i++)
+    {
+        std::cout << "*";
+    }
+    
+    std::cout << "\n";
+}
