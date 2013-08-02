fa12ba2506214817726b8be1359587745173913c
Kiet Lam <klam3518@gmail.com>
2013-08-02 16:29:18 -0700

    autotools to build helloworld

---
+++ autotools-helloworld/src/foofiles/foo.h
@@ -0,0 +1,10 @@
+#ifndef __FOO_H__
+#define __FOO_H__
+
+class Foo
+{
+public:
+    int GetTheNumber();
+};
+
+#endif // __FOO_H
---
+++ autotools-helloworld/src/main/hello.h
@@ -0,0 +1,13 @@
+#ifndef __HELLO_H__
+#define __HELLO_H__
+
+#include <iostream>
+#include <string>
+
+class Hello
+{
+public:
+    void HelloTest();
+};
+
+#endif // __HELLO_H__
